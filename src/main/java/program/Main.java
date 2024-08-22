c978f7dbba0ef6d255ce2ee9d02cfb7182ff0c05
Vinícius Santos Ribeiro <vinicius.santos.ribeiro.v@gmail.com>
2024-08-22 11:47:35 -0300

    idea

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,6 +1,7 @@
 package main.java.program;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
